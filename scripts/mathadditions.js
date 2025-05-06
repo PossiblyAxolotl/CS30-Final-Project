476d6a03e8ef6c46e1c610a06ffdd77bd79edae3
PossiblyAxolotl <me@possiblyaxolotl.com>
2025-05-06 14:16:39 -0600

    create first person camera movement and player object, replace deg2rad w/ arrow func & add clamp func in mathadditions

--- scripts/mathadditions.js
+++ scripts/mathadditions.js
@@ -1,3 +1,3 @@
-function deg2rad(degrees) {
-  return degrees * Math.PI / 180;
-}
+// Arrow notation functions, should be self explanatory
+const deg2rad = (degrees) => degrees * Math.PI / 180;
+const clamp = (val, min, max) => Math.min(Math.max(val, min), max); // keep val between min & max
--- sketch.js
+++ sketch.js
@@ -5,22 +5,44 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let cameraPosition = {
-  // position
-  x: 200,
-  y: -400,
-  z: 800,
-  
-  // rotation
-  rX: 0,
-  rY: 0,
-  rZ: 0,
-  
-  // look-at
-  lX: 201,
-  lY: -400,
-  lZ: 800,
-};
+class Player {
+  constructor(x=0, y=0, z=0, rY=0, rX=0) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    
+    this.rX = rX;
+    this.rY = rY;
+  }
+
+  process() {
+    this.lookAround();
+    this.displayCamera();
+  }
+
+  lookAround() {
+    this.rY -= movedX * sensitivity;
+    this.rX -= movedY * sensitivity;
+
+    clamp(this.rX, -90, 90);
+  }
+
+  moveWithInput() {
+    let i = input(); // scripts/input.js
+  }
+
+  displayCamera() {
+    // get a vector of what direction to look from rotation
+    let v = p5.Vector.fromAngles(deg2rad(this.rX),deg2rad(this.rY),1);
+
+    // look from current position in the direction of the vector
+    camera(this.x, this.y, this.z, this.x + v.x, this.y + v.y, this.z + v.z);
+  }
+}
+
+let sensitivity = 0.1;
+
+let player = new Player(100, -100, 400);
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -30,34 +52,13 @@ function setup() {
 function draw() {
   background(220);
 
-  //orbitControl();
-
-  moveCamera();
-  updateCamera();
+  player.process();
 
   push();
   rotateX(millis()/1000);
   rotateY(millis()/700);
   box(50);
   pop();
-}
-
-function moveCamera() {
-  let i = input();
-
-  cameraPosition.z += i.y * 10;
-}
-
-function mouseMoved() {
-  cameraPosition.rY += movedX;
-}
-
-/*
-Create a fake point that's located wherever the camera needs to look
-*/
-function updateCamera() {
-  let c = cameraPosition;
-  camera(c.x, c.y, c.z, c.lX, c.lY, c.lZ);
 }
 
 function mouseClicked() {
